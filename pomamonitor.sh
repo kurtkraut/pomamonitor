01b2b9c482ddacb9586441ac6efd228449339017
Thiago Ayub <dev@kurtkraut.net>
2009-11-01 04:15:40 -0200

    - Now using a conf file instead of making them on source code. - If on the first check everything is OK, user is warned that all hosts are online. - New flags: --version and --help

--- pomamonitor.sh
+++ pomamonitor.sh
@@ -13,66 +13,104 @@
 #    GNU General Public License for more details.
 #
 #    You should have received a copy of the GNU General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#    along with this program. If not, see <http://www.gnu.org/licenses/>.
 #
-#
-#Preencha abaixo entre as aspas e separado por espaços os hosts que quer monitorar
-#
-targets="www.terra.com.br www.uol.com.br www.google.com www.opendns.com www.registro.br www.alemdonatural.com.br loja.alemdonatural.com.br NS3.EU.EDITDNS.NET NS5.US.EDITDNS.NET NS4.US.EDITDNS.NET remoto.alemdonatural.com.br"
-#
-#Indique abaixo, entre as aspas, o intervalo em segundos entre uma checagem e outra de todos os hosts supracitados
-#
-normaldelay="300"
-#
-#Caso um dos hosts esteja offline, é interessante fazer checagens mais curtas. Nesse caso, elas serão feitas a cada X segundos, especificados abaixo entre aspas
-#
-briefdelay="60"
-#
-#Não modifique desta linha em diante
-#
-#
-#
-output ()
+clioutput ()
 {
 #Adiciona um timestamp ao echo. Forma padrão de stdout.
 time=$(date +%X)
 echo "($time) $*"
 }
-#Avisa ao usuário que o monitor está ativado.
-notify-send --icon=notification-network-ethernet-disconnected "Poor's Man Monitor" "Monitor ativado"
-output "Poor's Man Monitor - Monitor ativado"
-#Faz a primeira checagem dentro de 5 segundos:
+#--help flag
+if test "$1" = "--help"
+then
+ echo "\
+
+Usage: $0 [--help|--version|hosts]
+
+--help: Show this usage instructions.
+--version: Shows software version.
+hosts: Replace \"hosts\" by a list of hosts you want to monitor. If you do not
+provide at least one host at run time, a default list of hosts will be used.
+
+Example:
+
+$0 www.google.com www.opendns.com www.kernel.org
+
+"
+ exit 0
+fi
+#--version flag
+if test "$1" = "--version"
+then
+ echo "\
+
+This software is under constant and initial development. A version number
+would mean nothing at all. If you're going to distribute it and need to apply
+a number to this, we suggest calling it version 0.
+
+"
+ exit 0
+fi
+#Definindo arquivo de configuração e checando se há permissão para leitura.
+config_file=~/.pomamonitor/pomamonitor.conf
+if test -r $config_file
+then
+ clioutput "Using the configuration file $config_file"
+else
+ clioutput "Configuration file not found. Exiting. Place it on $config_file"
+ exit 1
+fi
+#Coletando variáveis a partir do $config_file
+targets=$(grep -G "^targets=" $config_file -m 1 | cut -f 2 -d"=")
+normal_delay=$(grep -G "^normal_delay=" $config_file -m 1 | cut -f 2 -d"=")
+brief_delay=$(grep -G "^brief_delay=" $config_file -m 1 | cut -f 2 -d"=")
+#Primeira checagem é feita com 5 segundos.
 delay=5
-#Utiliza hosts indicados na sintaxe (quando oferecidos)
+#Avisa ao usuário que o monitor está ativado.
+clioutput "Poor's Man Monitor - Monitor activated"
+clioutput "If you need further details on how to use this software, please type $0 --help"
+#Utiliza hosts indicados na sintaxe (quando oferecidos) e os exibe p/ o usuário
 if test -z $1
 then
- output Utilizando listas de host padrão: $targets
+ clioutput "Monitoring the default list of hosts: $targets"
 else
  targets="$*"
- output Utilizando a lista de hosts: $targets
-fi
+ clioutput "Monitoring this list of hosts: $targets"
+ fi
 #Loop de checagem dos hosts
 while [ 1 ]
 do
- output "Aguardando $delay segundos para efetuar testes."
+ counter=$(expr $counter + 1)
+ clioutput "Waiting $delay seconds to perform the test nº $counter."
  sleep $delay
- output Efetuando testes neste exato momento.
+ clioutput "Making tests right now."
  temporary=$(mktemp)
  fping -dAeu $targets > $temporary
  if test $? -eq 1
  then
 #Se houver pelo menos um host offline:
-  delay="$briefdelay"
-  output "Pausa entre uma checagem e outra temporariamente modificada para $delay segundos."
-  output=$(cat $temporary)
-  output "Dados coletados:"
+  delay="$brief_delay"
+  clioutput "The pause between each test was temporarily changed to $delay seconds."
+  results=$(cat $temporary)
+  clioutput "Collected data:"
   cat $temporary
-  notify-send --urgency=critical --icon=notification-network-ethernet-disconnected "Host fora do ar" "$output"
+  notify-send --urgency=critical --icon=notification-network-ethernet-disconnected "Offline hosts on check nº $counter" "$results"
  else
 #Se todos os hosts online:
   rm $temporary
-  delay="$normaldelay"
-  output Nenhum host detectado como offline.
+  delay="$normal_delay"
+  clioutput "No offline hosts were detected."
+#Se for o primeiro teste, avisar via notify-send que está todos hosts estão online.
+  if test $counter -eq 1
+  then
+   if test -z $1
+   then
+    notify-send --urgency=low --icon=notification-network-ethernet-connected "Poor's Man Monitor" "All default hosts are online and responding. New checks will be done on every $delay seconds."
+   else
+    notify-send --urgency=low --icon=notification-network-ethernet-connected "Poor's Man Monitor" "All hosts are online and responding. New checks will be done on every $delay seconds on: $targets"
+   fi
+  fi
  fi
- output Teste concluído.
+ clioutput "Current test finished."
 done
